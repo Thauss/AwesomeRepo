716571140428e6afdbcf2db8477616f11838f6ce
Thauss <edu.rojas.sanchez@gmail.com>
2014-03-31 06:53:18 +0200

    Nueva Rama Sobre la que seguir trabajando
    - Añadidas funcionalidades en swing
    
    - Problema al seleccionar solo directorios

---
+++ ProyectosGit/ordenaArchivos/interfaz/BuscaArchivos.java
@@ -0,0 +1,13 @@
+package ordenaArchivos.interfaz;
+
+public class BuscaArchivos {
+	
+	String ext;
+
+	public BuscaArchivos(String ext) {
+		super();
+		this.ext = ext;
+		
+	}
+
+}
--- ProyectosGit/ordenaArchivos/interfaz/OrdenaArchivos.java
+++ ProyectosGit/ordenaArchivos/interfaz/OrdenaArchivos.java
@@ -3,6 +3,7 @@ package ordenaArchivos.interfaz;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -15,9 +16,14 @@ import javax.swing.border.EmptyBorder;
 public class OrdenaArchivos extends JFrame {
 
 	private JPanel contentPane;
-	private JTextPane textPane;
-	private JButton btnSeleccionar;
+	private JTextPane TextAr;
+	private JButton botonSelA;
 	String ter;
+	private JButton botonSelOr;
+	private JButton botonSelDes;
+	private JTextPane textOrigen;
+	private JTextPane textDestino;
+	JFileChooser jf;
 
 	/**
 	 * Launch the application.
@@ -46,33 +52,98 @@ public class OrdenaArchivos extends JFrame {
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
-		textPane = new JTextPane();
-		textPane.setBounds(29, 72, 416, 35);
-		contentPane.add(textPane);
+		TextAr = new JTextPane();
+		TextAr.setText("Extension del archivo");
+		TextAr.setBounds(28, 122, 416, 35);
+		contentPane.add(TextAr);
 		
-		JLabel lblSeleccionaArchivo = new JLabel("Selecciona archivo");
-		lblSeleccionaArchivo.setBounds(29, 37, 135, 24);
-		contentPane.add(lblSeleccionaArchivo);
+		JLabel SelAr = new JLabel("Selecciona archivo");
+		SelAr.setBounds(28, 87, 135, 24);
+		contentPane.add(SelAr);
 		
-		btnSeleccionar = new JButton("Seleccionar");
-		btnSeleccionar.addActionListener(new ActionListener() {
+		botonSelA = new JButton("Seleccionar");
+		botonSelA.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				JFileChooser jf=new JFileChooser(".");
 				jf.showDialog(contentPane,"seleccion");
 				ter=jf.getSelectedFile().getName();
 				ter=ter.substring(ter.lastIndexOf('.')+1);
-				textPane.setText(ter.toUpperCase());
+				TextAr.setText(ter.toUpperCase());
 			}
 		});
-		btnSeleccionar.setBounds(141, 38, 89, 23);
-		contentPane.add(btnSeleccionar);
+		botonSelA.setBounds(355, 88, 89, 23);
+		contentPane.add(botonSelA);
+		
+		JLabel SelOr = new JLabel("Selecciona Carpeta donde buscar");
+		SelOr.setBounds(28, 168, 201, 23);
+		contentPane.add(SelOr);
+		
+		botonSelOr = new JButton("Seleccionar");
+		botonSelOr.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JFileChooser jf=new JFileChooser(".");
+				jf.showDialog(contentPane,"seleccion");
+				ter=jf.getSelectedFile().getName();
+				ter=ter.substring(ter.lastIndexOf('.')+1);
+				TextAr.setText(ter.toUpperCase());
+			}
+		});
+		botonSelOr.setBounds(355, 168, 89, 23);
+		contentPane.add(botonSelOr);
+		
+		JLabel Titulo = new JLabel("ORGANIZADOR DE ARCHIVOS 1.0");
+		Titulo.setBounds(28, 30, 175, 14);
+		contentPane.add(Titulo);
+		
+		JLabel SelDes = new JLabel("Selecciona Carpeta donde copiar");
+		SelDes.setBounds(28, 248, 201, 23);
+		contentPane.add(SelDes);
+		
+		botonSelDes = new JButton("Seleccionar");
+		botonSelOr.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				jf=new JFileChooser(".");
+				jf.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				 jf.setAcceptAllFileFilterUsed(false);
+				jf.showDialog(contentPane,"seleccion");
+				
+				String ar=(jf.getCurrentDirectory().getAbsolutePath());
+				textOrigen.setText(ar);
+				}
+			
+		});
+		botonSelDes.setBounds(355, 248, 89, 23);
+		contentPane.add(botonSelDes);
+		
+		textOrigen = new JTextPane();
+		textOrigen.setText("Ruta de Busqueda");
+		textOrigen.setBounds(28, 202, 416, 35);
+		contentPane.add(textOrigen);
+		
+		textDestino = new JTextPane();
+		textDestino.setText("Ruta de Destino");
+		textDestino.setBounds(28, 282, 416, 35);
+		contentPane.add(textDestino);
 		
 		
 	}
 	public JTextPane getTextPane() {
-		return textPane;
+		return TextAr;
 	}
 	public JButton getBtnSeleccionar() {
-		return btnSeleccionar;
+		return botonSelA;
+	}
+	public JButton getBotonSelOr() {
+		return botonSelOr;
+	}
+	public JButton getBotonSelDes() {
+		return botonSelDes;
+	}
+	public JTextPane getTextOrigen() {
+		return textOrigen;
+	}
+	public JTextPane getTextDestino() {
+		return textDestino;
 	}
 }
